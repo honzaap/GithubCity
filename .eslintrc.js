d6efa2092e1fe2afff105f2c207006fa60e96c0d
eggplants <w10776e8w@yahoo.co.jp>
2022-07-19 08:06:05 +0900

    Added linter configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-    "indent": ["error", 4],
-  }
-}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["standard", "prettier"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["simple-import-sort", "import"],
+    rules: {
+        indent: ["error", 4],
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "import/first": "error",
+        "import/newline-after-import": "error",
+        "import/no-duplicates": "error",
+    },
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  tabWidth: 4,
-  trailingComma: 'all',
+    tabWidth: 4,
+    trailingComma: "all",
 };
