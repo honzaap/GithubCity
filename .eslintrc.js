231008e77726e2c354915c83b571b2bcb650e48a
eggplants <w10776e8w@yahoo.co.jp>
2022-07-19 07:15:33 +0900

    Added linter/formatter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    "indent": ["error", 4],
+  }
+}
