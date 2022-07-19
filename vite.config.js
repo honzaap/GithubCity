ad466458b0a89505dd72995d9df8ec573763ba39
eggplants <w10776e8w@yahoo.co.jp>
2022-07-20 06:46:32 +0900

    fix: remove useless comment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
-        host: true, // IPアドレスを有効化
+        host: true,
     },
     root: "./src",
     base: "/GithubCity/",
